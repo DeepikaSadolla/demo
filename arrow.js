21f68bbab89fd3e8c411dd482f29d013b3853aff
DeepikaSadolla <124116810+DeepikaSadolla@users.noreply.github.com>
2023-02-01 12:09:43 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,11 @@
+// const sum=(a,b)=>a+b;
+// console.log(sum(2,3));
+// console.log(sum(5,10));
+
+
+// const sum=(x,y)=>
+// {
+//  return x+y;
+// }
+// console.log(sum(10,5));
+
---
+++ demo1.js
@@ -0,0 +1,62 @@
+/*var x=5;
+var x=10;
+console.log(x);*/
+// let aname='deepika'
+// let aname='dinesh'
+/*let aname='dinesh'
+var aname='dinesh'*/
+// var aname=123;
+// {
+// var aname=456;
+
+// console.log(aname);
+// }
+// console.log(aname)
+// const x=8;
+//  x=10;
+// if(true){
+//     const func=function(){
+//            x=10;
+//        console.log(x);
+//     }
+//     console.log(x);
+//   func();
+// }
+
+
+
+// function dem(){
+//     for(let i=0;i<=10;i++){
+//         console.log(i);
+//     }
+// }
+// dem();
+// console.log(i);-->exception
+
+
+// let val=10;
+// function test(){
+//     let val=5;
+//     if(val==5){
+//         let val=7;
+//         console.log(val);
+//     }
+//     console.log(val);
+// }
+// test();
+// console.log(val);
+
+const val=10;
+function test(){
+    var val=5;
+    if(val==5){
+        let val=7;
+        console.log(val);
+    }
+    console.log(val);
+}
+test();
+console.log(val);
+
+
+
